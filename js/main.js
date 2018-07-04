2dba08f84daf2deae5d9293a0de1f0f38a7b96e9
Qi Long <BaiQiLongMa@gmail.com>
2018-07-04 15:40:45 -0400

    updated for correct date

--- js/main.js
+++ js/main.js
@@ -1,18 +1,78 @@
 $(function () {
 
     var dateObj = new Date();
-    var month = dateObj.getUTCMonth() + 1; 
-    var day = dateObj.getUTCDate();
-    var yesterDay = dateObj.getUTCDate() - 7;
-    var year = dateObj.getUTCFullYear();
-    var todayDate = year + "-0" + month + "-" + day;
-    yesterdayDate = year + "-0" + month + "-" + yesterDay;
+    var month = dateObj.getMonth() + 1; 
+    var lastMonth = dateObj.getMonth();
+    var dayOfMonth = dateObj.getDate();
+    var year = dateObj.getFullYear();
+
+   function dateWeekAgo(){ 
+    var yesterDate = dayOfMonth -7;    
+    
+   if(dayOfMonth <= 7){
+    var remainderABS = function(){
+    //gets absolute value of number so you can minus it from the previous month date
+    return Math.abs(dayOfMonth -7);
+    }
+    
+    var dateSevenDaysAgo;
+ 
+switch(lastMonth) {
+    case 0:
+    case 2:
+    case 4:
+    case 6:
+    case 7:
+    case 9:
+    case 11:              
+       dateSevenDaysAgo = 30 - remainderABS();
+       break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+       dateSevenDaysAgo = 30 - remainderABS();
+        break;        
+        case 2:
+        
+        if(dayOfMonth <= 28){
+         dateSevenDaysAgo = 28 - remainderABS();
+        }else{
+        //leap year
+        dateSevenDaysAgo = 29 - remainderABS();
+        }
+        break;
+    default:
+        $("#near-asteroid-count").text("NASA we have a problem! The API must be down");
+}
+    
+    return dateSevenDaysAgo;
+      } 
+      else{ return yesterDate;}
+    }
+
+    var singleOrDoubleDigitMonth = function(){
+        if(month < 10){
+        return "0" + month;
+        }else{
+        return month;
+        }
+        }
+        
+ var isLastMonth = function(){
+     if (dayOfMonth <= 7) {
+         return lastMonth;
+     }
+        else{singleOrDoubleDigitMonth();}
+ }
+    
+    var todayDate = year + "-" + singleOrDoubleDigitMonth() + "-" + dayOfMonth;
+    yesterdayDate = year + "-" + isLastMonth() + "-" + dateWeekAgo();
 
     // My Key from NASA is "PmFsOBkLS9R3L1WYnp7sMzxFZWGWQF8TwdmYNpNX" 
     var asteroidURL = "https://api.nasa.gov/neo/rest/v1/feed?start_date=" + yesterdayDate + "&end_date=" + todayDate + "&api_key=PmFsOBkLS9R3L1WYnp7sMzxFZWGWQF8TwdmYNpNX";
-
-    //another idea would have been to put variables in the date for the asteroidURL then output the span of 7 days that the user can input, but focusing more on the table of the names in browser-asteroids.js
  
         $.getJSON(asteroidURL).then(function (asteroidNumber) {$("#near-asteroid-count").text(JSON.stringify(asteroidNumber.element_count, null, 0))});
+
 });
 
